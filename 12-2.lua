83199079383caee15dfa251c2bcfae8c4a362121
Buds <mrbouyou@gmail.com>
2021-12-12 18:50:57 +0100

    day 12

---
+++ 12-2.lua
@@ -0,0 +1,48 @@
+-- https://adventofcode.com/2021/day/12
+
+local nodes = {}
+
+local function new_node(a, b)
+  nodes[a] = nodes[a] or {}
+  local node = nodes[a]
+  if node.smallcave == nil then
+    local first_char = a:sub(1,1)
+    node.smallcave = first_char >= "a" and first_char <= "z"
+  end
+  if b then
+    node.children = node.children or {}
+    if not node.children[b] then
+      node.children[b] = true
+      new_node(b, a)
+    end
+  end
+end
+
+for line in io.lines("12.input") do
+  for a,b in line:gmatch("([^%-]+)-([^%-]+)") do
+    new_node(a, b)
+  end
+end
+
+local function all_routes_from(a,finish, visited, joker_visit)
+  if a == finish then return 1 end
+  local local_visited = {}
+  for k in pairs(visited) do local_visited[k] = true end
+  if nodes[a].smallcave then
+    if local_visited[a] then
+      if joker_visit == false and a ~= "start" and a ~= "end" then
+        joker_visit = true
+      else
+        return 0
+      end
+    end    
+    local_visited[a] = true
+  end
+  local count = 0
+  for name in pairs(nodes[a].children) do
+    count = count + all_routes_from(name, finish, local_visited, joker_visit)
+  end
+  return count
+end
+
+print(all_routes_from("start","end",{},false))
